9b7893da0a80a0f901345bbf03c08b71bbae077a
Tumantaev Baiaman <baiaman.tumantaev.kk@gmail.com>
2022-05-18 18:57:37 +0600

    create-image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    title: '',
+    image: null,
+    video: null,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  };
+
+  handleImageChange = (e) => {
+    this.setState({
+      image: e.target.files[0]
+    })
+  };
+  handleVideoChange = (e) => {
+    this.setState({
+      video: e.target.files[0]
+    })
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+    let form_data = new FormData();
+    // form_data.append('category', this.state.category);
+    form_data.append('title', this.state.title);
+    form_data.append('img', this.state.image, this.state.image.name);
+    form_data.append('video_test', this.state.video, this.state.video.name);
+    // form_data.append('content', this.state.content);
+    let url = ('http://127.0.0.1:8000/posts/');
+    axios.post(url, form_data, {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    })
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err => console.log(err))
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <input type="text" placeholder='Title' id='title' value={this.state.title} onChange={this.handleChange} required/>
+          </p>
+          
+            {/* <select defaultValue="School" placeholder='Caategory' id='category' onChange={this.handleChange} value={this.state.category}>
+                    <option value=""></option>
+                    <option value={5}>Category</option>
+            </select> */}
+          <p>
+            <input type="file"
+                   id="image"
+                   accept="image/png, image/jpg"  onChange={this.handleImageChange} required/>
+          </p>
+          <p>
+            <input type="file"
+                   id="video"
+                   accept="video/mp4"  onChange={this.handleVideoChange} required/>
+          </p>
+          <input type="submit"/>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
